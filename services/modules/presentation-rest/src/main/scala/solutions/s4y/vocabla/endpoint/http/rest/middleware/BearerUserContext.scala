6e0c9afceb7df39ff84dfbf30b7b5ea387f03e8f
Sergey Dolin <sergey.dolin@gmail.com>
2025-08-30 21:25:57 +0200

    refactor: simplify BearerUserContext by removing unused l18n

--- services/modules/presentation-rest/src/main/scala/solutions/s4y/vocabla/endpoint/http/rest/middleware/BearerUserContext.scala
+++ services/modules/presentation-rest/src/main/scala/solutions/s4y/vocabla/endpoint/http/rest/middleware/BearerUserContext.scala
@@ -1,22 +1,15 @@
 package solutions.s4y.vocabla.endpoint.http.rest.middleware
 
-import solutions.s4y.i18n.ResourcesStringsResolver.default
-import solutions.s4y.i18n.t
 import solutions.s4y.vocabla.app.ports.GetUserUseCase
-import solutions.s4y.vocabla.app.ports.errors.ServiceFailure
 import solutions.s4y.vocabla.domain.identity.Identifier.identifier
 import solutions.s4y.vocabla.domain.{User, UserContext}
 import solutions.s4y.vocabla.endpoint.http.rest.error.HttpError
 import solutions.s4y.vocabla.endpoint.http.rest.error.HttpError.*
-import solutions.s4y.vocabla.endpoint.http.rest.middleware.BrowserLocale.withLocale
 import zio.ZIO
 import zio.http.*
 
-import java.util.Locale
-
 object BearerUserContext:
-  val bearerAuthWithContext
-      : HandlerAspect[GetUserUseCase & Locale, UserContext] =
+  val bearerAuthWithContext: HandlerAspect[GetUserUseCase, UserContext] =
     HandlerAspect.interceptIncomingHandler(Handler.fromFunctionZIO[Request] {
       request =>
         (request.header(Header.Authorization) match {
