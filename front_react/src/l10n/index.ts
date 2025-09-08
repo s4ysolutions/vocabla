29a8ad038c18da824a007ed03e2cdcf18ca634e9
Sergey Dolin <sergey.dolin@gmail.com>
2025-09-08 21:25:03 +0200

    Fix undefined values in l10n

--- front_react/src/l10n/index.ts
+++ front_react/src/l10n/index.ts
@@ -25,13 +25,13 @@ const translate = (key: string): string => messages[key] ? messages[key] : key;
 
 const init = (lc: string, dict: { [key: string]: Dictionary }) => {
   const lcs: string[] = lc.split('-');
-  const [mainlocale] = lcs;
+  const mainlocale = lcs[0]!;
   locale = lcs[1] ? `${lcs[0]}-${lcs[1].toUpperCase()}` : lc;
 
   const locales: string[] = Object.keys(dict);
   if (locales.length > 0) {
     // default messages
-    messages = {...dict[locales[0]]};
+    messages = {...dict[locales[0]!]};
     if (mainlocale !== locale) {
       // add messages from first part of locale
       if (dict[mainlocale]) {
@@ -62,7 +62,7 @@ const l10n = (strings: TemplateStringsArray, ...values: string[]): string => {
 
 export default l10n;
 
-export const byLang = (lc: string, key: string): string => dictionary[lc][key] ? dictionary[lc][key] : `L10N:${key}[${lc}]:L10N`;
+export const byLang = (lc: string, key: string): string => dictionary[lc]![key] ? dictionary[lc]![key] : `L10N:${key}[${lc}]:L10N`;
 
 import translations from './translations';
 
