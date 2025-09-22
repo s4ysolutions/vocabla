d5cdd31ebc91b779a78d80d121c7a7692dae3688
Sergey Dolin <sergey.dolin@gmail.com>
2025-09-22 20:32:47 +0200

    feat: add loglevel for enhanced debugging

--- front_react/src/react/main.tsx
+++ front_react/src/react/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import log from 'loglevel'
+
+log.setLevel('debug')
+log.getLogger('render').setLevel('debug')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
