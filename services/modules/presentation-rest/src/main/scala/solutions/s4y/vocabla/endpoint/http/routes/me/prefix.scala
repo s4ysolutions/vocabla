b6cc61dcaeedaa84343012f3471af5296bcb0b32
Sergey Dolin <sergey.dolin@gmail.com>
2025-09-25 20:36:13 +0200

    feat: remove empty Me endpoint

--- services/modules/presentation-rest/src/main/scala/solutions/s4y/vocabla/endpoint/http/routes/me/prefix.scala
+++
@@ -1,6 +0,0 @@
-package solutions.s4y.vocabla.endpoint.http.routes.me
-
-import solutions.s4y.vocabla.endpoint.http.routes
-import zio.http.codec.PathCodec
-
-val prfix: PathCodec[Unit] = routes.prefix / "me"
