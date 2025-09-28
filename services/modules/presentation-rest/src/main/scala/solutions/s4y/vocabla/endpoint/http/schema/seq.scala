599fc087b443cb837427b663d9fa05a1b2e323fc
Sergey Dolin <sergey.dolin@gmail.com>
2025-09-28 12:57:25 +0200

    feat: remove depricated schema

--- services/modules/presentation-rest/src/main/scala/solutions/s4y/vocabla/endpoint/http/schema/seq.scala
+++
@@ -1,9 +0,0 @@
-package solutions.s4y.vocabla.endpoint.http.schema
-
-import zio.schema.Schema
-
-given [A: Schema](using listSchema: Schema[List[A]]): Schema[Seq[A]] =
-  listSchema.transform(
-    (list: List[A]) => list.toSeq,
-    (seq: Seq[A]) => seq.toList
-  )
